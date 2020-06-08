83ce66e56241520eb3d95624154299d6ca036736
nans <nans.noel@gmail.com>
2020-06-08 22:21:57 +0200

    # Issue 6
    Ajout du mailer - Dev Open Letter
    Ajout dans le gemfile de Faker

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+  #
+  config.action_mailer.default_url_options = { host: 'form-you.herokuapp.com', port: 3000 }
+  config.active_storage.service = :local
+  config.action_mailer.delivery_method = :smtp
+
 end
