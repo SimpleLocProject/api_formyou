ab458e65b0ada93d4621a9b0c350a3c8769b35c0
DoubleLama <lama31@hotmail.fr>
2020-06-08 15:09:04 +0200

    belongs to added

--- db/schema.rb
+++ db/schema.rb
@@ -17,8 +17,10 @@ ActiveRecord::Schema.define(version: 2020_06_08_115055) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.bigint "course_categories_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["course_categories_id"], name: "index_categories_on_course_categories_id"
   end
 
   create_table "jwt_blacklists", force: :cascade do |t|
