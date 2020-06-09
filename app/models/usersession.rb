49821d1fc6db1902e29c30e40fe92a08cfb33bf7
DoubleLama <lama31@hotmail.fr>
2020-06-09 15:08:12 +0200

    only one usersession by day for one student

--- app/models/usersession.rb
+++ app/models/usersession.rb
@@ -2,4 +2,16 @@ class Usersession < ApplicationRecord
   belongs_to :student, class_name: "User", foreign_key: "student_id"
   belongs_to :session
   validates :note, presence: true 
+  before_create :one_at_once
+
+  def one_at_once
+    @studentUsersessions = Usersession.where(student_id: (User.find(self.student_id)))
+    
+    @studentUsersessions.each do |userSession| 
+      if (userSession.session.begin_date == self.session.begin_date)
+        throw :abort
+      end
+    end
+  end
+
 end
